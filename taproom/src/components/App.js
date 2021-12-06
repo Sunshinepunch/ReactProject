7b0a517a08032dd673d2d5ac9fe4a783d96da391
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-12-06 12:31:01 -0800

    updated component files and finally fixed component typo preventing compiling

---
+++ taproom/src/components/App.js
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import KegControl from "./KegControl";
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <KegControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ taproom/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header(){
+  return (
+    <h1>Tap Room Keg List</h1>
+  );
+}
+
+export default Header;
