d2f7d79f2a94efaf33acd5dd4a956a37b43cb751
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-12-06 13:07:26 -0800

    contain resusable form

---
+++ taproom/src/components/ReusableForm.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+      <label for="names">Name:</label>
+          <input type="text" name="name" placeholder="Kombourgeoisie" className="form-control"/>
+          <br />
+          <label for="brand">Brand:</label>
+          <input type="text" name="brand" placeholder="Oregon's Homegrown" className="form-control"/>
+          <br/>
+          <label for="price">Price:</label>
+          <input type="number" name="price" placeholder="$7.99" className="form-control"/>
+          <br/>
+          <label for="flavor">Flavor:</label>
+          <input type="text" name="flavor" placeholder="Ruby Crystal Orange" className="form-control"/>
+          <br/>
+          <label for="quantity">Pints Per Keg:</label>
+          <input type="number" name="quantity" placeholder="124 pints" className="form-control"/>
+          <br />
+          <button type="submit" className="btn">Submit</button>
+          <br />
+        </form>
+      </React.Fragment>
+
+  )
+}
+
+ReuseableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func
+}
+
+export default ReusableForm;
